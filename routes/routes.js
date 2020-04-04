ccf08529ddad2ba55869e0e102af38acec29bd85
pankaj3571 <53375656+pankaj3571@users.noreply.github.com>
2020-04-04 18:18:20 +0530

    crud done

--- routes/routes.js
+++ routes/routes.js
@@ -5,5 +5,7 @@ var registerFile=require('../controllers/register');
 router.post('/registerdata',registerFile.register);
 router.get('/getData',registerFile.getData);
 router.post('/login',registerFile.login)
+router.post('/update',registerFile.update)
+router.post('/delete',registerFile.deleteData)
 
 module.exports=router;
